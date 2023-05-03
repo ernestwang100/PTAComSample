95ba9b99e079b83d252f0ec4e20950c406382064
Yu-En Wang <ernestwang100@gmail.com>
2023-05-03 14:33:14 +0800

    upBtn, downBtn, homeBtn for SettingActivity add, adjust activity_setting.xml recyclerview constraint

--- app/src/main/java/com/asuka/ptacomsample/SettingActivity.java
+++ app/src/main/java/com/asuka/ptacomsample/SettingActivity.java
@@ -1,6 +1,10 @@
 package com.asuka.ptacomsample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,25 +14,51 @@ import java.util.ArrayList;
 
 public class SettingActivity extends AppCompatActivity {
     ArrayList<SettingModel> settingModels = new ArrayList<>();
-
+    RecyclerView recyclerView;
+    Button homeBtn, upBtn, downBtn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        RecyclerView recyclerView = findViewById(R.id.settingrecycle);
+        recyclerView = findViewById(R.id.settingrecycle);
+        homeBtn = findViewById(R.id.homeBtn);
+        upBtn = findViewById(R.id.upBtn);
+        downBtn = findViewById(R.id.downBtn);
 
         setSettingModels();
 
         SettingAdapter settingAdapter = new SettingAdapter(this, settingModels);
         recyclerView.setAdapter(settingAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        homeBtn.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setClass(SettingActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+        upBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                recyclerView.scrollBy(0, -100);
+            }
+        });
+
+        downBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                recyclerView.scrollBy(0, 100);
+            }
+        });
     }
+
 
     private void setSettingModels() {
         String[] settingNames = getResources().getStringArray(R.array.setting_full_txt);
 
+        Log.d("settingNames", String.valueOf(settingNames.length));
         for (int i = 0; i < settingNames.length; i++) {
             settingModels.add(new SettingModel(settingNames[i]));
         }
