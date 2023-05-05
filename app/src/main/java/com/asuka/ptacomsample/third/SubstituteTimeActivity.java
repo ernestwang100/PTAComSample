8a542b0c3e35a311368d03680aa8c23dde996527
Yu-En Wang <ernestwang100@gmail.com>
2023-05-05 15:26:23 +0800

    adjust recylcerview layout, add SubstituteTimeActivity

--- app/src/main/java/com/asuka/ptacomsample/third/SubstituteTimeActivity.java
+++ app/src/main/java/com/asuka/ptacomsample/third/SubstituteTimeActivity.java
@@ -9,26 +9,27 @@ import android.widget.Button;
 import com.asuka.ptacomsample.R;
 import com.asuka.ptacomsample.second.SettingActivity;
 
-public class DrivingTimeActivity extends AppCompatActivity {
+public class SubstituteTimeActivity extends AppCompatActivity {
+
     Button homeBtn, upBtn, downBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driving_time);
+        setContentView(R.layout.activity_substitute_time);
         homeBtn = findViewById(R.id.homeBtn);
         upBtn = findViewById(R.id.upBtn);
         downBtn = findViewById(R.id.downBtn);
 
         homeBtn.setOnClickListener(v -> {
             Intent intent = new Intent();
-            intent.setClass(DrivingTimeActivity.this, SettingActivity.class);
+            intent.setClass(SubstituteTimeActivity.this, SettingActivity.class);
             startActivity(intent);
         });
 
-        downBtn.setOnClickListener(v -> {
+        upBtn.setOnClickListener(v -> {
             Intent intent = new Intent();
-            intent.setClass(DrivingTimeActivity.this, SubstituteTimeActivity.class);
+            intent.setClass(SubstituteTimeActivity.this, DrivingTimeActivity.class);
             startActivity(intent);
         });
 
