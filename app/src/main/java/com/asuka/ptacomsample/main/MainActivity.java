a257b7730c2340418ecaa8eccf615f615ddd3e87
Yu-En Wang <ernestwang100@gmail.com>
2023-07-20 16:04:50 +0800

    add loadTheme function to MainActivity (still have bugs)

--- app/src/main/java/com/asuka/ptacomsample/main/MainActivity.java
+++ app/src/main/java/com/asuka/ptacomsample/main/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements ButtonFreezeListe
     private RecvThread mRecvThread;
     private byte[] writeData;
     private String[] temp;
+    private boolean isDarkTheme;
 
     public MainActivity(){
         super();
@@ -45,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements ButtonFreezeListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        switchTheme();
+//        loadTheme();
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tv);
         themeSW = findViewById(R.id.themeSwitchButton);
@@ -64,18 +68,23 @@ public class MainActivity extends AppCompatActivity implements ButtonFreezeListe
 
         mRecvThread = new RecvThread(handler, mPort, writeData, this,this);
         mRecvThread.start();
-        themeSW.setOnClickListener(v -> {
-            runOnUiThread(() -> {
-                if (themeSW.isChecked()) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                }
-                recreate();
-            });
-        });
+//        themeSW.setOnClickListener(v -> {
+//            runOnUiThread(() -> {
+//                if (themeSW.isChecked()) {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                } else {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                }
+//                recreate();
+//            });
+//        });
 
-//        Switch themeSwitchButton = findViewById(R.id.themeSwitchButton);
+        Switch themeSwitchButton = findViewById(R.id.themeSwitchButton);
+
+        themeSwitchButton.setOnClickListener(v -> {
+            toggleTheme();
+            recreate();
+        });
 //
 //        themeSwitchButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
 //            if (!isChecked) {
@@ -86,7 +95,6 @@ public class MainActivity extends AppCompatActivity implements ButtonFreezeListe
 //            recreate();
 //        });
 
-//        switchTheme();
 
         mainMenuBtn.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, SettingListActivity.class);
@@ -126,29 +134,51 @@ public class MainActivity extends AppCompatActivity implements ButtonFreezeListe
 //            }, 1000); // 1000 milliseconds = 1 second delay
         });
     }
+    private void toggleTheme() {
+        isDarkTheme = !isDarkTheme;
+        saveTheme();
+    }
 
-//    private void switchTheme() {
-//        // Retrieve the current theme
-//        int currentTheme = getCurrentTheme();
-//
-//        // Determine the new theme based on the current theme
-//        int newTheme = currentTheme == R.style.Theme_PTAComSample_Light
-//                ? R.style.Theme_PTAComSample_Dark
-//                : R.style.Theme_PTAComSample_Light;
-//
-//        // Set the new theme for the activity
-//        setTheme(newTheme);
-//
-//        // Recreate the activity to apply the new theme
-//        recreate();
-//    }
-//
-//    private int getCurrentTheme() {
-//        // Retrieve the current theme resource ID of the activity
-//        TypedValue typedValue = new TypedValue();
-//        getTheme().resolveAttribute(androidx.appcompat.R.attr.theme, typedValue, true);
-//        return typedValue.resourceId;
-//    }
+    private void loadTheme() {
+        SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        isDarkTheme = sharedPreferences.getBoolean("dark_theme", false);
+        if (isDarkTheme) {
+            setTheme(R.style.Theme_PTAComSample_Light);
+        } else {
+            setTheme(R.style.Theme_PTAComSample_Dark);
+        }
+    }
+
+    private void saveTheme() {
+        SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("dark_theme", isDarkTheme);
+        editor.apply();
+    }
+
+
+    private void switchTheme() {
+        // Retrieve the current theme
+        int currentTheme = getCurrentTheme();
+
+        // Determine the new theme based on the current theme
+        int newTheme = currentTheme == R.style.Theme_PTAComSample_Light
+                ? R.style.Theme_PTAComSample_Dark
+                : R.style.Theme_PTAComSample_Light;
+
+        // Set the new theme for the activity
+        setTheme(newTheme);
+
+        // Recreate the activity to apply the new theme
+        recreate();
+    }
+
+    private int getCurrentTheme() {
+        // Retrieve the current theme resource ID of the activity
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(androidx.appcompat.R.attr.theme, typedValue, true);
+        return typedValue.resourceId;
+    }
 
 
     @Override
