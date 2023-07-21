7e3bf94c12ab2acae670cecb3d34343d02205b51
Yu-En Wang <ernestwang100@gmail.com>
2023-07-21 16:35:26 +0800

    add hexInputFilter in DriverStatusFragment

--- app/src/main/java/com/asuka/ptacomsample/third/DriverStatusFragment.java
+++ app/src/main/java/com/asuka/ptacomsample/third/DriverStatusFragment.java
@@ -9,7 +9,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.Patterns;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -19,6 +25,7 @@ import com.asuka.ptacomsample.main.RecvThread;
 
 public class DriverStatusFragment extends Fragment {
     private RadioGroup radioGroupDriverStatus, radioGroupCodriverStatus;
+    private EditText driverCodeEdt, codriverCodeEdt;
     private Integer driverStatus, codriverStatus;
 
     private String cmd, temp[], cmdStart;
@@ -36,6 +43,8 @@ public class DriverStatusFragment extends Fragment {
 
         radioGroupDriverStatus = view.findViewById(R.id.DRBtnGroup);
         radioGroupCodriverStatus = view.findViewById(R.id.CRBtnGroup);
+        driverCodeEdt = view.findViewById(R.id.driverCodeEdt);
+        codriverCodeEdt = view.findViewById(R.id.codriverCodeEdt);
 
         radioGroupDriverStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -52,6 +61,32 @@ public class DriverStatusFragment extends Fragment {
                 Log.d(TAG, "onCheckedChanged: codriverStatus: " + codriverStatus);
             }
         });
+
+        // Create an input filter to restrict input to hexadecimal characters (0-9, A-F, a-f)
+        InputFilter hexInputFilter = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                // Define the regular expression pattern for hexadecimal characters (0-9, A-F, a-f)
+                String regexPattern = "[0-9A-Fa-f]+";
+
+                // Combine the existing text with the new text being inserted
+                String newText = dest.subSequence(0, dstart) + source.toString() + dest.subSequence(dend, dest.length());
+
+                // Check if the new text matches the pattern (i.e., only contains 0-9, A-F, or a-f)
+                if (!newText.matches(regexPattern)) {
+                    // Reject the new input by returning an empty string (no changes will be made to the EditText)
+                    Toast.makeText(getContext(), "Only hexadecimal characters are allowed", Toast.LENGTH_SHORT).show();
+                    return "";
+                }
+
+                // If the input is valid, allow it to be inserted into the EditText
+                return null;
+            }
+        };
+
+        // Set the input filter to the driverCodeEdt and codriverCodeEdt EditTexts
+        driverCodeEdt.setFilters(new InputFilter[]{hexInputFilter});
+        codriverCodeEdt.setFilters(new InputFilter[]{hexInputFilter});
 
         return view;
     }
