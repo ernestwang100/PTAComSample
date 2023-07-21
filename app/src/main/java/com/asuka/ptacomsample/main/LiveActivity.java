fa04e4187d6d4b9249d8dfe15b6ddb6684cd13c6
Yu-En Wang <ernestwang100@gmail.com>
2023-07-21 09:42:00 +0800

    add LiveActivity

---
+++ app/src/main/java/com/asuka/ptacomsample/main/LiveActivity.java
@@ -0,0 +1,71 @@
+package com.asuka.ptacomsample.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.asuka.comm.ComPort;
+import com.asuka.ptacomsample.R;
+import com.asuka.ptacomsample.second.SettingListActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class LiveActivity extends AppCompatActivity implements ButtonFreezeListener {
+    private static final String TAG = "LiveActivity";
+    private Switch themeSW;
+    private FloatingActionButton listFAB;
+    private TextView tv;
+    private Handler handler;
+    private ComPort mPort;
+    private RecvThread mRecvThread;
+    private byte[] writeData;
+
+    public LiveActivity(){
+        super();
+        mPort = new ComPort();
+        mPort.open(5, ComPort.BAUD_115200, 8, 'N', 1);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live);
+        tv = findViewById(R.id.tv);
+        themeSW = findViewById(R.id.themeSwitchButton);
+        listFAB = findViewById(R.id.listFAB);
+        writeData = "$LCD+PAGE=0".getBytes();
+        mRecvThread = new RecvThread(handler, mPort, writeData, this,this);
+        mRecvThread.start();
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                tv.setText(new String((byte[]) msg.obj));
+            }
+        };
+
+        listFAB.setOnClickListener(v -> {
+            mRecvThread.interrupt();
+            Intent intent = new Intent(this, SettingListActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+
+    @Override
+    public void freezeButtons() {
+
+    }
+
+    @Override
+    public void unfreezeButtons() {
+
+    }
+}
